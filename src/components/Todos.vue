2d06e286238eaec92053e1c186ac2972963b5764
osc98 <70422327+osc98@users.noreply.github.com>
2021-06-27 09:06:37 -0500

    img

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -28,7 +28,7 @@
       <div tabindex="0" class="e-card" id="basic"  data-aos="fade-up" data-aos-delay="500">
         <div class="e-card-header">
             <div class="e-card-header-caption">
-                <div class="e-card-title">Grounded airplanes during the novel coronavirus pandemic</div>
+                <div class="e-card-title"><h1>Airplanes during the novel coronavirus pandemic</h1></div>
             </div>
         </div>
         <div class="e-card-content">
@@ -55,23 +55,45 @@ remote sensing data – synthetic aperture radar (SAR) data from JAXA’s ALOS-2
 <br>
 <br>
 <br>
+<div id='container' style="margin:0px auto 0; width:50%;">
+    <br>
+      <div tabindex="0" class="e-card" id="basic"  data-aos="fade-up" data-aos-delay="500">
+        <div class="e-card-content">
+          •	By comparing SAR images of airports before, during, and after lockdowns, scientists can determine how air traffic may have changed during the pandemic.
+•	In general, manmade targets such as buildings, cars, and airplanes appear brighter in SAR imagery than natural targets like bare land, forested areas, or the ocean.
+
+        </div>
+    </div>
+</div> 
 <br>
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<br><div id='container' style="margin:0px auto 0; width:50%;">
+    <br>
+      <div tabindex="0" class="e-card" id="basic"  data-aos="fade-up" data-aos-delay="500">
+        <div class="e-card-header">
+            <div class="e-card-header-caption">
+                <div class="e-card-title"><h1>North Adriatic - Water Quality Regional Maps</h1></div>
+            </div>
+        </div>
+        
+    </div>
+</div> 
+<br><div id='container' style="margin:0px auto 0; width:50%;">
+ <div tabindex="0" class="e-card" id="basic"  data-aos="fade-up" data-aos-delay="500">
+        <div class="e-card-header">
+            <div class="e-card-header-caption">
+                <div class="e-card-title"><h1>Airplanes during the novel coronavirus pandemic</h1></div>
+            </div>
+        </div>
+        <div class="e-card-content">
+          •	To help quantify these changes, scientists are combining two kinds of space-based
+remote sensing data – synthetic aperture radar (SAR) data from JAXA’s ALOS-2 and ESA’s Copernicus Sentinel-1 satellites with NASA-processed high-resolution optical remote sensing data from Planet Inc. ESA's Copernicus Sentinel-2 multispectral satellite data – to observe and quantify the changes in grounded airplanes and air traffic around the world
+
+        </div>
+    </div>
+    </div>
 <br>
 <br>
 <br>
@@ -172,6 +194,11 @@ export default {
       if (window.scrollY > 920 && window.scrollY < 940 ) {
         if (!this.defaultImage) {
           return (this.isDefaultImage = "https://raw.githubusercontent.com/osc98/dajao22/main/src/assets/A10.jpg",this.isDefaultImageTam=500);
+        }
+      }
+      if (window.scrollY > 1000 ) {
+        if (!this.defaultImage) {
+          return (this.isDefaultImage = "https://raw.githubusercontent.com/osc98/dajao22/main/src/assets/A10.jpg",this.isDefaultImageTam=0);
         }
       }
           },
